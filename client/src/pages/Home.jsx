35b6aa3d93a65ee92421d402a92fa8ac1de92a21
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-09-23 16:48:06 +0530

    bg is removed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,15 +4,12 @@ import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 
 function Home() {
   return (
-    <div className="overflow-hidden absolute">
-      <img src="/bg.jpg" alt="" className='w-screen h-screen object-cover'/>
       <Link to='/users' className="absolute top-6 right-8">
-            <button className="btn btn-outline border-black text-black py-0 shadow-lg flex items-center">
+            <button className="btn btn-outline btn-accent  text-white py-0 shadow-lg flex items-center">
               <span className="text-xl">ENTER</span>
-              <MdOutlineKeyboardDoubleArrowRight  className="size-10 animate" />
+              <MdOutlineKeyboardDoubleArrowRight  className="size-8 animate" />
             </button>
       </Link>
-    </div>
   )
 }
 
