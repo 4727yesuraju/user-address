4b2e491d3df49735031bc1e1e99ab535d298139a
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-09-23 08:55:17 +0530

    server testing is completed

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
-console.log("hia")
+import express from 'express';
+
+
+const app = express();
+
+//localhost:8080/test
+app.get('/test',(req,res)=>{
+    res.send("Hello from server :)");
+});
+
+const PORT = 8080;
+app.listen(PORT,()=>{
+    console.log(`server is running at ${PORT}`);
+})
