9ac517a47d3ed374ece1257860018b550652ec47
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-09-23 09:16:56 +0530

    user and address model are created

---
+++ api/model/adress.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const addressSchema = new mongoose.Schema({
+    address : {
+        type : String,
+        required : true,
+    } ,
+    city : {
+        type : String,
+        required : true,
+    } ,
+    state : {
+        type : String,
+        required : true,
+    } ,
+    country : {
+        type : String,
+        required : true,
+    },
+    pincode : {
+        type : String,
+        required : true
+    }
+},{timestamps : true});
+
+const Address = mongoose.model("Address",addressSchema);
+
+export default Address ; 
---
+++ api/model/user.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    address : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'Address',
+            default : [],
+        }
+    ]
+},{timestamps : true});
+
+const User = mongoose.model("User",userSchema);
+
+export default User ; 
