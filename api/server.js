7f21a9199ab767dc0942aaccec6a47da6cfbfdba
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-09-23 09:02:02 +0530

    dotenv file is added

--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import {config} from 'dotenv';
+config(); //to access env variable
 
 
 const app = express();
@@ -8,7 +10,7 @@ app.get('/test',(req,res)=>{
     res.send("Hello from server :)");
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT,()=>{
     console.log(`server is running at ${PORT}`);
 })
